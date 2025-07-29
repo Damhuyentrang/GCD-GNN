460455ce361547bd004c7a86c16f9f029587ab61
Damhuyentrang <damtrang102@gmail.com>
2025-07-29 16:49:16 +0700

    android studio

---
+++ app/src/main/java/com/academy/mbank/Application.java
@@ -0,0 +1,15 @@
+package com.academy.mbank;
+
+import com.academy.mbank.model.User;
+
+public class Application {
+    public static long money = 100000000;
+    public static String username= "DAM THI HUYEN TRANG";
+    public static String phone= "0886010204";
+    public static String password= "01022004";
+
+
+    public static User userCurrent;
+
+
+}
